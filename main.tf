70b86afb5a773a27807559c10c1646b4ac93be1e
NaushadPasha <33582239+NaushadPasha@users.noreply.github.com>
2021-11-23 16:30:03 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,9 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "my-s3-bucket[count.index]" {
+resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
   acl = var.acl
-  count = 3
   
    versioning {
     enabled = var.versioning
